6191cc5f148a0d6817e7c5f393a1af6a89f42960
adam <adam.t61vnuhus@gmail.com>
2021-05-23 23:33:35 +0700

    bootrap data

--- Cinema/src/main/java/com/onemount/cinema/repository/CinemaRepository.java
+++ Cinema/src/main/java/com/onemount/cinema/repository/CinemaRepository.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Integer> {
+
+    Optional<Cinema> findByName(String name);
 }
